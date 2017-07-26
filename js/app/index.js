7c4596550df2ad42c3c002b8d7caf3c5d97615f6
Eazerchen <rock8salt@gmail.com>
2017-07-26 11:29:07 +0800

    Add files via upload

---
+++ js/app/index.js
@@ -0,0 +1,5 @@
+define( ['jquery','com/carousel','com/gotop','com/waterfall'], function ($,Carousel,GoTop,WaterFall) {
+    Carousel.init($('.carousel'));
+    new GoTop;
+    new WaterFall($('.ct-waterfall'))
+})
---
+++ js/app/main.js
@@ -0,0 +1,8 @@
+requirejs.config({
+    baseUrl: 'js',
+    paths: {
+        'jquery': 'com/jquery.min'
+    }
+});
+
+requirejs(['app/index'])
